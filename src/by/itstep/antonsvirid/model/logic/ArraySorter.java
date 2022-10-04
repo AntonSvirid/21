e37fc754138173e0acdf1c16a9f12a1a47511c1e
AntonSvirid <adsvirid@tut.by>
2022-10-04 10:54:38 +0300

    add selectedSort and tests for him

--- src/by/itstep/antonsvirid/model/logic/ArraySorter.java
+++ src/by/itstep/antonsvirid/model/logic/ArraySorter.java
@@ -1,17 +1,40 @@
 package by.itstep.antonsvirid.model.logic;
 
 public class ArraySorter {
+    public static void selectedSortAsc(int[] array) {      // алгоритм сортировки выбора на осн
+
+        if (array == null) {
+            throw new RuntimeException();
+        }
+
+        for (int j = 0; j < array.length -1; j++) {
+
+        int indexMinElement = j;
+
+        for (int i = j + 1; i < array.length; i++) {        // находление экстремальнго значения min
+            if (array[indexMinElement] > array[i]) {
+                indexMinElement = i;
+            }
+        }
+
+        int t = array[j];
+        array[j] = array[indexMinElement];
+        array[indexMinElement] = t;
+    }
+
+}
+
     public static void bubbleSortAsc(int[] array) { // по возрастанию - ASC
 
-        int count = 0;
+        int count = 0;          // debug - счётчик дебага, инициализация
 
         if (array == null) {
             throw new RuntimeException();
         }
 
         for (int j = 0; j < array.length - 1; j++) {
-            for (int i = 0; i < array.length - 1; i++) {
-                count++;                //  счётчик дебага во внутренний цикл
+            for (int i = 0; i < array.length - 1 - j; i++) {
+                count++;                //  debug - счётчик дебага во внутренний цикл
                 if (array[i] > array[i + 1]) {
                     int t = array[i];
                     array[i] = array[i + 1];
@@ -19,6 +42,29 @@ public class ArraySorter {
                 }
             }
         }
-        System.out.println("count = " + count);      //debug
+        System.out.println("count = " + count);      //debug - визуализация
     }
+
+    public static void bubbleSortDesc(int[] array) { // по убыванию - Desc
+        int count = 0;          // debug - счётчик дебага, инициализация
+
+        if (array == null) {
+            throw new RuntimeException();
+        }
+
+        for (int j = 0; j < array.length - 1; j++) {
+            for (int i = 0; i < array.length - 1 - j; i++) {
+                count++;                //  debug - счётчик дебага во внутренний цикл
+                if (array[i] < array[i + 1]) {
+                    int t = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = t;
+                }
+            }
+        }
+        System.out.println("count = " + count);      //debug - визуализация
+    }
+
+
 }
+
--- test/by/itstep/antonsvirid/model/logic/ArraySorterTest.java
+++ test/by/itstep/antonsvirid/model/logic/ArraySorterTest.java
@@ -8,8 +8,8 @@ public class ArraySorterTest {
     @Test
     public void testBubbleSortAsc() {
 
-        int[] array = {7, 2, 3, 4, 4};
-        int[] expected = {2, 3, 4, 4, 7};
+        int[] array = {7, 2, 3, 8, 7, 5};
+        int[] expected = {2, 3, 5, 7, 7, 8};
 
         ArraySorter.bubbleSortAsc(array);
 
@@ -32,4 +32,57 @@ public class ArraySorterTest {
         ArraySorter.bubbleSortAsc(new int[0]);
     }
 
+    @Test
+    public void bubbleSortDescTest() {
+
+        int[] array = {7, 2, 3, 8, 7, 5};
+        int[] expected = {8, 7, 7, 5, 3, 2};
+
+        ArraySorter.bubbleSortDesc(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != expected[i]) {
+                fail();
+            }
+        }
+    }
+
+    @Test(expected = RuntimeException.class)        // негативный тест-кейс
+    public void testBubbleSortDescWithNull() {       // ожидающий данную ошибку
+
+        ArraySorter.bubbleSortAsc(null);
+    }
+
+    @Test
+    public void testBubbleSortDescWithEmptyObject() {
+
+        ArraySorter.bubbleSortAsc(new int[0]);
+    }
+
+    @Test
+    public void testSelectedSortAsc() {
+
+        int[] array = {7, 2, 3, 8, 7, 5};
+        int[] expected = {2, 3, 5, 7, 7, 8};
+
+        ArraySorter.selectedSortAsc(array);
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != expected[i]) {
+                fail();     // вызов ошибки при несовпадении данных
+            }               // метод fail из библ Assert
+        }
+    }
+
+    @Test(expected = RuntimeException.class)        // негативный тест-кейс
+    public void testSelectedSortAscWithNull() {       // ожидающий данную ошибку
+
+        ArraySorter.selectedSortAsc(null);
+    }
+
+    @Test
+    public void testSelectedSortAscWithEmptyObject() {
+
+        ArraySorter.selectedSortAsc(new int[0]);
+    }
 }
